53039f38e806815a77fd78447147429498d85b31
devcldkai229 <kag40222@gmail.com>
2025-05-28 02:30:18 +0700

    Test Commit

---
+++ backend/FertilityCareApplication/FertilityCare.API/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FertilityCare.API.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
